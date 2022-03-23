ba5cde318fcb8e70d47ecd4f389841b28a15fce0
Tony Lu <TonyLu@Tonys-iMac.fios-router.home>
2022-03-23 05:59:00 -0400

    fixed a typo

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 const server = require('./server');
 
-const port = procdss.env.PORT || 5000;
+const port = process.env.PORT || 5000;
 
 server.listen(port, () => {
     console.log('server listening on ', port);
